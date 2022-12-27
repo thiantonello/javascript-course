1a4914e0246adecd7d1561de2ef9f6059d0b101c
Thiago Antonello Vargas <thiantonello@gmail.com>
2022-12-27 19:16:10 -0300

    js fundamentals part 1

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,39 @@
+let js = 'amazing'
+if (js === 'amazing') alert('Javascript is fun!!')
+
+console.log(40 + 8 + 23 - 10)
+
+console.log(js)
+
+// Values and Variables
+const country = 'Brasil'
+const continent = 'America'
+let population = 220_000_000
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+console.log('_____________________')
+
+// Data Types
+const isIsland = false
+let language
+
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
+
+console.log('_____________________')
+
+// let, const and var
+language = 'Portuguese'
+console.log(language)
+// if isIsland = true, the code will not work because isIsland is a const
+// const variables are, like the name suggests, constants. Their values cannot be changed
+// Uncaught TypeError: Assignment to constant variable.
+// var declarations are global or function-scoped while let and const are block-scoped
+// var and let variables can be redeclared and updated
+
+console.log('_____________________')
