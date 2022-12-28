a71e371ce1c23a8ee7aa8f6b988943e159d00d1d
Thiago Antonello Vargas <thiantonello@gmail.com>
2022-12-27 22:33:15 -0300

    js fundamentals part 1 - variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,11 +1,13 @@
 let js = 'amazing'
 if (js === 'amazing') alert('Javascript is fun!!')
 
+/*
 console.log(40 + 8 + 23 - 10)
 
 console.log(js)
 
-// Values and Variables
+//----------------------------------------------
+//Values and Variables
 const country = 'Brasil'
 const continent = 'America'
 let population = 220_000_000
@@ -14,9 +16,12 @@ console.log(country)
 console.log(continent)
 console.log(population)
 
-console.log('_____________________')
+//----------------------------------------------
+//Data Types
+//  JavaScript has a dynamic typing: We do not have to manually define the data type of the value stored in a variable. They are defined automatically
 
-// Data Types
+//  It's important to notice that the variable does not have a type. The VALUE stored inside the variable has a type.
+
 const isIsland = false
 let language
 
@@ -25,15 +30,32 @@ console.log(typeof population)
 console.log(typeof country)
 console.log(typeof language)
 
-console.log('_____________________')
+language = true
+console.log('the type of the data stored in language is', typeof language)
+language = 12312
+console.log('the type of the data stored in language is', typeof language)
 
-// let, const and var
+//
+//  There are seven primitive data types:
+//    Number      (always floating point numbers)
+//    String      (sequence of characters)
+//    Boolean     (logical type == true or false)
+//    Undefined   (value taken by a variable that is not defined)
+//    Null
+//    Symbol      (value that is unique and cannot be changed)
+//    BigInt      (larger integers than the Number type can hold)
+//
+
+//----------------------------------------------
+//let, const and var
 language = 'Portuguese'
 console.log(language)
-// if isIsland = true, the code will not work because isIsland is a const
-// const variables are, like the name suggests, constants. Their values cannot be changed
-// Uncaught TypeError: Assignment to constant variable.
-// var declarations are global or function-scoped while let and const are block-scoped
-// var and let variables can be redeclared and updated
 
-console.log('_____________________')
+//  if we try to set isIsland = true, the code will not work because isIsland is a const
+//  const variables always demand an initial value
+//  const variables are, like the name suggests, constants. Their values cannot be changed. If we try to change a const variable we gonna get the following error:  Uncaught TypeError: Assignment to constant variable.
+//
+//  var declarations are global or function-scoped while let and const are block-scoped
+//  var and let variables can be redeclared and updated
+
+*/
