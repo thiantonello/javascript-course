fb1c2fbbcb59404fdc4c213021215fe1b04d8cb7
Thiago Antonello Vargas <thiantonello@gmail.com>
2023-01-16 15:01:19 -0300

    js 2 - coding challenge 1

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -259,7 +259,7 @@ function describePopulation({ countryName, population }) {
 
 countries.forEach((country) => console.log(describePopulation(country)))
 
-*/
+
 //----------------------------------------------------------------------------//
 //Reviewing Functions
 
@@ -307,3 +307,70 @@ const calcAge3 = (birthYear) => new Date().getFullYear() - birthYear
 //  3. The parameters
 //  4. The function body
 //  5. The return statement
+
+*/
+//----------------------------------------------------------------------------//
+//Coding Challenge #1
+
+//  Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+//  Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+//  A team only wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+//  1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+//  2. Use the function to calculate the average for both teams
+//  3. Create a function 'checkWinner' that takes the average score of each team
+//  as parameters ('avgDolphins' and 'avgKoalas'), and then logs the winner to
+//  the console, together with the victory points, according to the rule above.
+//  Example: "Koalas win (30 vs. 13)"
+//  4. Use the 'checkWinner' function to determine the winner for both Data 1
+//  and Data 2
+//  5. Ignore draws this time
+
+//  TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+//  TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+const dolphinsGames = [
+  [44, 23, 71],
+  [85, 54, 41],
+]
+const koalasGames = [
+  [65, 54, 49],
+  [23, 34, 27],
+]
+
+let round
+
+const calcAverage = function (games) {
+  let score = 0
+
+  games[round].forEach((game) => (score += game))
+
+  return score / games.length
+}
+
+function checkWinner(roundNumber) {
+  setRound(roundNumber)
+
+  let avgDolphins = calcAverage(dolphinsGames)
+  let avgKoalas = calcAverage(koalasGames)
+
+  if (avgDolphins >= avgKoalas * 2) {
+    return console.log(
+      `Round ${round}, Dolphins wins (${avgDolphins} vs. ${avgKoalas})`
+    )
+  } else if (avgKoalas >= avgDolphins * 2) {
+    return console.log(
+      `Round ${round}, Koalas wins (${avgKoalas} vs. ${avgDolphins})`
+    )
+  } else {
+    console.log(`Round ${round}, No winner`)
+  }
+}
+
+function setRound(roundNumber) {
+  round = roundNumber
+}
+
+checkWinner(0)
+console.log(' ')
+checkWinner(1)
