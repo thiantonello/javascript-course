b066b8ab4beeb378749e3b0428dd04255ba80d72
Thiago Antonello Vargas <thiantonello@gmail.com>
2023-01-16 23:01:01 -0300

    js 2 - coding challenge 2

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -548,7 +548,6 @@ if (friends.includes('Steven')) {
   console.log('You have a friend called Steven')
 }
 
-*/
 //  Assignments:
 //  LECTURE: Basic Array Operations (Methods)
 //  1. Create an array containing all the neighboring countries of a country of your choice. Choose a country which has at least 2 or 3 neighbors. Store the array into a variable called 'neighbors'
@@ -589,3 +588,41 @@ if (!neighbors.includes('Germany')) {
 neighbors[neighbors.indexOf('San Marino')] = 'Brazil'
 
 console.log(neighbors)
+
+*/
+//----------------------------------------------------------------------------//
+//Coding Challenge #2
+
+//  Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+//  Your tasks:
+//  1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+
+//  2. And now let's use arrays! So create an array 'bills' containing the test data below
+
+//  3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before
+
+//  4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+
+//  Test data: 125, 555 and 44
+
+const bills = [100, 125, 555, 44]
+
+const total = []
+
+const tips = []
+
+function calcTip(bills) {
+  bills.forEach((bill) => {
+    const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+    tips.push(Number(tip.toFixed(2)))
+
+    total.push(Number((bill + tip).toFixed(2)))
+  })
+}
+
+calcTip(bills)
+
+console.log(bills)
+console.log(tips)
+console.log(total)
